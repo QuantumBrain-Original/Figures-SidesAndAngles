448b8b8c2615c4bd5e527d706de91fd4bf1981a4
QuantumBrain-Original <Quantum.Brain.Original@gmail.com>
2024-11-27 01:40:12 +0300

    Virtual methods added

--- Figures-SidesAndAngles.cpp
+++ Figures-SidesAndAngles.cpp
@@ -1,129 +1,136 @@
 ﻿#include <iostream>
-//#include <string>
 #include <Windows.h>
 
 class Figure
 {
 	protected:
-		unsigned int a, b, c, d, A, B, C, D;
+		unsigned int sides_count;
 	public:
-		int get_angle_A()
+		virtual void print_output()
 		{
-			return A;
+			std::cout << "Стороны: Нет\n" << "Углы: Нет\n" << std::endl;
 		}
 
-		int get_angle_B()
-		{
-			return B;
-		}
-
-		int get_angle_C()
-		{
-			return C;
-		}
-
-		int get_angle_D()
-		{
-			return D;
-		}
-
-		int get_side_A()
-		{
-			return a;
-		}
-
-		int get_side_B()
-		{
-			return b;
-		}
-
-		int get_side_C()
-		{
-			return c;
-		}
-
-		int get_side_D()
-		{
-			return d;
-		}
-
-		Figure(int sideA = 0, int sideB = 0, int sideC = 0, int sideD = 0, int angleA = 0, int angleB = 0, int angleC = 0, int angleD = 0) :
-			a(sideA), b(sideB), c(sideC), d(sideD), A(angleA), B(angleB), C(angleC), D(angleD) {}
+		Figure() : sides_count(0) {}
 };
 
 // фигуры с 3 углами
 class Triangle : public Figure
 {
+	protected:
+		unsigned int a, b, c, A, B, C;
 	public:
-		Triangle(int sideA, int sideB, int sideC, int angleA, int angleB, int angleC) : Figure(sideA, sideB, sideC, 0, angleA, angleB, angleC, 0) {}
+		void print_output() override
+		{
+			if (sides_count == 3)
+			{
+				std::cout << "Стороны: a=" << a << " b=" << b << " c=" << c << std::endl;
+				std::cout << "Углы: A=" << A << " B=" << B << " C=" << C << std::endl;
+			}
+		}
+
+		Triangle(int sideA = 0, int sideB = 0, int sideC = 0, int angleA = 0, int angleB = 0, int angleC = 0) :
+			a(sideA), b(sideB), c(sideC), A(angleA), B(angleB), C(angleC)
+		{
+			sides_count = 3;
+		}
 };
 
 class RectangularTriangle : public Triangle // Прямоугольный треугольник (угол C всегда равен 90)
 {
 	public:
-		RectangularTriangle(int sideA, int sideB, int sideC, int angleA, int angleB) : Triangle(sideA, sideB, sideC, angleA, angleB, 90) {}
+		RectangularTriangle(int sideA = 0, int sideB = 0, int sideC = 0, int angleA = 0, int angleB = 0) :
+			Triangle(sideA, sideB, sideC, angleA, angleB, 90)
+		{
+			sides_count = 3;
+		}
 };
 
 class IsoscelesTriangle : public Triangle // Равнобедренный треугольник (стороны a и c равны, углы A и C равны)
 {
 	public:
-		IsoscelesTriangle(int sideA, int sideB, int angleA, int angleB) : Triangle(sideA, sideB, sideA, angleA, angleB, angleA) {}
+		IsoscelesTriangle(int sideA = 0, int sideB = 0, int angleA = 0, int angleB = 0) :
+			Triangle(sideA, sideB, sideA, angleA, angleB, angleA)
+		{
+			sides_count = 3;
+		}
 };
 
 class EquilateralTriangle : public Triangle // Равносторонний треугольник (все стороны равны, все углы равны 60)
 {
 	public:
-		EquilateralTriangle(int sideA) : Triangle(sideA, sideA, sideA, 60, 60, 60) {}
+		EquilateralTriangle(int sideA = 0) : Triangle(sideA, sideA, sideA, 60, 60, 60)
+		{
+			sides_count = 3;
+		}
 };
 
 // фигуры с 4 углами
 class Quadrangle : public Figure // Четырехугольник
 {
+	protected:
+		unsigned int a, b, c, d, A, B, C, D;
 	public:
-		Quadrangle(int sideA, int sideB, int sideC, int sideD, int angleA, int angleB, int angleC, int angleD) : Figure(sideA, sideB, sideC, sideD, angleA, angleB, angleC, angleD) {}
+		void print_output() override
+		{
+			if (sides_count == 4)
+			{
+				std::cout << "Стороны: a=" << a << " b=" << b << " c=" << c << " d=" << d << std::endl;
+				std::cout << "Углы: A=" << A << " B=" << B << " C=" << C << " D=" << D << std::endl;
+			}
+		}
+
+		Quadrangle(int sideA = 0, int sideB = 0, int sideC = 0, int sideD = 0, int angleA = 0, int angleB = 0, int angleC = 0, int angleD = 0) :
+			a(sideA), b(sideB), c(sideC), d(sideD), A(angleA), B(angleB), C(angleC), D(angleD)
+		{
+			sides_count = 4;
+		}
 };
 
 class Parallelogram : public Quadrangle // Параллелограмм (стороны a,c и b,d попарно равны, углы A,C и B,D попарно равны)
 {
 	public:
-		Parallelogram(int sideA, int sideB, int angleA, int angleB) : Quadrangle(sideA, sideB, sideA, sideB, angleA, angleB, angleA, angleB) {}
+		Parallelogram(int sideA = 0, int sideB = 0, int angleA = 0, int angleB = 0) :
+			Quadrangle(sideA, sideB, sideA, sideB, angleA, angleB, angleA, angleB)
+		{
+			sides_count = 4;
+		}
 };
 
 class Rhombus : public Parallelogram // Ромб (все стороны равны, углы A,C и B,D попарно равны)
 {
 	public:
-		Rhombus(int sideA, int angleA, int angleB) : Parallelogram(sideA, sideA, angleA, angleB) {}
+		Rhombus(int sideA = 0, int angleA = 0, int angleB = 0) :
+			Parallelogram(sideA, sideA, angleA, angleB)
+		{
+			sides_count = 4;
+		}
 };
 
 class Rectangles : public Parallelogram // Прямоугольник (стороны a,c и b,d попарно равны, все углы равны 90)
 {
 	public:
-		Rectangles(int sideA, int sideB) : Parallelogram(sideA, sideB, 90, 90) {}
+		Rectangles(int sideA = 0, int sideB = 0) : Parallelogram(sideA, sideB, 90, 90)
+		{
+			sides_count = 4;
+		}
 };
 
 class Square : public Rectangles // Квадрат (все стороны равны, все углы равны 90)
 {
 	public:
-		Square(int sideA) : Rectangles(sideA, sideA) {}
+		Square(int sideA = 0) : Rectangles(sideA, sideA)
+		{
+			sides_count = 4;
+		}
 };
 
 void print_info(Figure* objFigure)
 {
-	std::cout << "Стороны:";
-	std::cout << " a=" << objFigure->get_side_A();
-	std::cout << " b=" << objFigure->get_side_B();
-	std::cout << " c=" << objFigure->get_side_C();
-	if(objFigure->get_side_D() > 0)
-		std::cout << " d=" << objFigure->get_side_C();
-	std::cout << "\n";
-
-	std::cout << "Углы:";
-	std::cout << " A=" << objFigure->get_angle_A();
-	std::cout << " B=" << objFigure->get_angle_B();
-	std::cout << " C=" << objFigure->get_angle_C();
-	if (objFigure->get_angle_D() > 0)
-		std::cout << " D=" << objFigure->get_angle_D();
-	std::cout << "\n";
+	if (objFigure != nullptr)
+		objFigure->print_output();
+	else
+		std::cout << "Ошибка чтения указателя!\n";
 }
 
 int main() // Задача 2. Фигуры. Стороны и углы
@@ -132,10 +139,14 @@ int main() // Задача 2. Фигуры. Стороны и углы
 	SetConsoleOutputCP(1251);
 	setlocale(LC_ALL, "Russian");
 
+	Figure* unkFigure = new Figure{};
 	Triangle* triengle = new Triangle{10, 20, 30, 50, 60, 70};
 	RectangularTriangle* rectangularTriangle = new RectangularTriangle{ 10, 20, 30, 50, 60 }; // Прямоугольный треугольник
 	IsoscelesTriangle* isoscelesTriangle = new IsoscelesTriangle{ 10, 20, 50, 60 }; // Равнобедренный треугольник
 	EquilateralTriangle* equilateralTriangle = new EquilateralTriangle{30}; // Равносторонний треугольник
+
+	std::cout << "Фигура:" << std::endl;
+	print_info(unkFigure);
 
 	std::cout << "Треугольник:" << std::endl;
 	print_info(triengle);
@@ -153,6 +164,7 @@ int main() // Задача 2. Фигуры. Стороны и углы
 	delete isoscelesTriangle;
 	delete rectangularTriangle;
 	delete triengle;
+	delete unkFigure;
 
 	Quadrangle *quadrangle = new Quadrangle{ 10, 20, 30, 40, 50, 60, 70, 80 }; // Четырехугольник
 	Parallelogram* parallelogram = new Parallelogram{20, 30, 30, 40}; // Параллелограмм
@@ -180,5 +192,4 @@ int main() // Задача 2. Фигуры. Стороны и углы
 	delete rhombus;
 	delete parallelogram;
 	delete quadrangle;
-
 }
